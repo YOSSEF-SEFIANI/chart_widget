5372669207b30d9ea851dc4a44114a8c9fd1a121
SefianiYoussef <sefianiy10@gmail.com>
2026-01-26 17:21:22 +0100

    Refactor navigation.tsx for improved code clarity and consistency

--- src/setting/settings/components/navigation.tsx
+++ src/setting/settings/components/navigation.tsx
@@ -1,31 +1,63 @@
-import { React, classNames } from 'jimu-core'
-import { SettingOutlined } from 'jimu-icons/outlined/application/setting'
-import { Button, utils } from 'jimu-ui'
+import { React, classNames } from "jimu-core";
+import { SettingOutlined } from "jimu-icons/outlined/application/setting";
+import { Button } from "jimu-ui";
 
 export interface NavigationProps {
-  level?: 1 | 2 | 3
-  title: string
-  active?: boolean
-  stickRight?: boolean
-  onClick: () => void
-  className?: string
+  level?: 1 | 2 | 3;
+  title: string;
+  active?: boolean;
+  stickRight?: boolean;
+  onClick: () => void;
+  className?: string;
 }
 
-export const Navigation = React.forwardRef((props: NavigationProps, ref: React.RefObject<HTMLButtonElement>): React.ReactElement => {
-  const { level = 3, title, active, onClick, className, stickRight = false } = props
+const getLevelClass = (level: number) => {
+  switch (level) {
+    case 1:
+      return "text-level-1";
+    case 2:
+      return "text-level-2";
+    case 3:
+    default:
+      return "text-level-3";
+  }
+};
 
-  return (
-    <div
-    className={classNames(
+export const Navigation = React.forwardRef<HTMLButtonElement, NavigationProps>(
+  (props, ref) => {
+    const {
+      level = 3,
+      title,
+      active,
+      onClick,
       className,
-      'navigation w-100 d-flex align-items-center justify-content-between',
-        utils.getSettingTextClassesMap(level)
-    )}
-    >
-      <span className='title'>{title}</span>
-      <Button className={classNames({ 'pr-0': stickRight })} ref={ref} aria-label={title} size='sm' type='tertiary' active={active} icon onClick={onClick}>
-        <SettingOutlined />
-      </Button>
-    </div>
-  )
-})
+      stickRight = false,
+    } = props;
+
+    return (
+      <div
+        className={classNames(
+          className,
+          "navigation w-100 d-flex align-items-center justify-content-between",
+          getLevelClass(level),
+        )}
+      >
+        <span className="title">{title}</span>
+        <Button
+          className={classNames({ "pr-0": stickRight })}
+          ref={ref}
+          aria-label={title}
+          size="sm"
+          type="tertiary"
+          active={active}
+          icon
+          onClick={onClick}
+        >
+          <SettingOutlined />
+        </Button>
+      </div>
+    );
+  },
+);
+
+Navigation.displayName = "Navigation";
